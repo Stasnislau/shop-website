62961f19a8fac9795dce154afe2e4b5a11ddef85
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-06-29 23:19:40 +0200

    added cartGTO

--- server/src/cart/cart/cart.controller.ts
+++ server/src/cart/cart/cart.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Delete,
+  Put,
+  Param,
+} from "@nestjs/common";
+import { CartService } from "./cart.service";
+import { CartDTO } from "../dto";
 
-@Controller('cart')
-export class CartController {}
+@Controller("cart")
+export class CartController {
+  constructor(private cartService: CartService) {}
+  @Post("add")
+  addToCart(@Body() body: CartDTO) {
+    // this.cartService.addToCart(body);
+  }
+  @Get("all")
+  getAllCartItems() {
+    // this.cartService.getAllCartItems();
+  }
+
+  @Delete("delete/:id")
+  deleteCartItem(@Param("id") id: number) {
+    // this.cartService.deleteCartItem(id);
+  }
+
+  @Put("update/:id")
+  updateCartItem(@Param("id") id: number, @Body() body: CartDTO) {
+    // this.cartService.updateCartItem(id, body);
+  }
+}
---
+++ server/src/cart/dto/index.ts
@@ -0,0 +1,14 @@
+import { ProductDTO } from "../../products/dto";
+export interface CartDTO {
+    id : number;
+    cartItems : CartItemDTO[];
+}
+
+export interface CartItemDTO {
+    id : number;
+    productId : number;
+    quantity : number;
+    cartId : number;
+    product : ProductDTO;
+}
+
