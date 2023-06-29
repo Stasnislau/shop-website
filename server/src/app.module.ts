200332ac3d540a2c2ba828058aa64c37d234fe36
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-06-29 21:39:07 +0200

    added skeleton for cart

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ProductsModule } from "./products/products.module";
 import { PrismaModule } from './prisma/prisma.module';
+import { CartModule } from "./cart/cart/cart.module";
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, CartModule],
   controllers: [],
   providers: [],
 })
