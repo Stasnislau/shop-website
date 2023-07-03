9df678ff500a6f01ff26a0620d57572da96d75ed
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-07-03 18:33:24 +0200

    added skeleton for controller

--- server/src/cart-item/cart-item.controller.ts
+++ server/src/cart-item/cart-item.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
+import { CartItemService } from "./cart-item.service";
 
-@Controller('cart-item')
-export class CartItemController {}
+@Controller("cart-item")
+export class CartItemController {
+  constructor(private cartItemService: CartItemService) {}
+  getCartItem() {
+    // this.cartItemService.getCartItem();
+  }
+  createCartItem() {
+    // this.cartItemService.createCartItem();
+  }
+  updateCartItem() {
+    // this.cartItemService.updateCartItem();
+  }
+  deleteCartItem() {
+    // this.cartItemService.deleteCartItem();
+  }
+}
--- server/src/cart-item/cart-item.service.ts
+++ server/src/cart-item/cart-item.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CartItemService {}
+export class CartItemService {
+    constructor() {}
+    
+}
