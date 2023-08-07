fafe8c9139cd9a97cc175efc7ccfd7a0090e9660
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-08-07 21:30:27 +0300

    updated server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { json, urlencoded } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: "http://localhost:3000" }); // CORS
+  app.use(json({ limit: "50mb" }));
+  app.use(urlencoded({ extended: true, limit: "50mb" }));
   await app.listen(3001);
 }
 bootstrap();
