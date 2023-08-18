079337d399fb887f097b47fd45556221067b270a
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-08-18 16:35:41 +0200

    updated itemLoadingComponent

--- src/components/itemLoadingComponent.tsx
+++ src/components/itemLoadingComponent.tsx
@@ -1,20 +1,54 @@
-import { Box } from "@mui/material";
+import { Height } from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Skeleton,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const ItemLoadingComponent = () => {
   return (
-    <Box sx={
-      {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
+    <Card
+      sx={{
         width: "100%",
-        border: "1px solid black",
-      }
-    }>
-      <span className="item-loader"></span>
-    </Box>
+        height: "100%",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          padding: 1,
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+          }}
+          height="80%"
+        >
+          <Skeleton variant="rectangular" height="100%" />
+        </Box>
+        <CardContent
+          sx={{
+            padding: "0.5rem",
+            height: "20%",
+          }}
+        >
+          <Box>
+            <Typography fontSize="1rem">
+              <Skeleton />
+            </Typography>
+            <Typography fontSize="1rem" color="text.secondary">
+              <Skeleton />
+            </Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 
