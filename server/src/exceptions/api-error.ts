6e7bd79824d7ee0fac8bc5af7b31c722b8f09d5c
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-08-23 17:35:09 +0200

    implemented apiError class

---
+++ server/src/exceptions/api-error.ts
@@ -0,0 +1,31 @@
+class ApiError extends Error {
+  status: number;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+
+  static badRequest(message: string) {
+    return new ApiError(400, message);
+  }
+  static unauthorized(message: string) {
+    return new ApiError(401, message);
+  }
+
+  static forbidden(message: string) {
+    return new ApiError(403, message);
+  }
+  static notFound(message: string) {
+    return new ApiError(404, message);
+  }
+  static internal(message: string) {
+    return new ApiError(500, message);
+  }
+  static notImplemented(message: string) {
+    return new ApiError(501, message);
+  }
+  static serverUnavailable(message: string) {
+    return new ApiError(503, message);
+  }
+}
