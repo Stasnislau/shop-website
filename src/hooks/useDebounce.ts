58216e655851068b66d1d2c8a082f4c0641ccdfd
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-08-31 00:18:12 +0200

    implemented useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+
+const useDebounce = (func: Function, delay: number) => {
+  const debounce = useRef<ReturnType<typeof setTimeout>>();
+  return (...args: any) => {
+    if (debounce.current) {
+        console.log("clearing timeout");
+        clearTimeout(debounce.current);}
+    debounce.current = setTimeout(() => func(...args), delay);
+  };
+};
+
+export default useDebounce;
